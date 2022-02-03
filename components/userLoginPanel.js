5010558291fb767c7b97c3e12498ca59d21831d5
Ancubed <antonov.andrew.jr@gmail.com>
2022-02-03 11:58:06 +0500

    Вращение иконки при открытом меню

--- components/userLoginPanel.js
+++ components/userLoginPanel.js
@@ -1,6 +1,5 @@
 import { useSession } from "next-auth/react"
 
-import { Fragment } from 'react'
 import { Menu, Transition } from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/solid'
 
@@ -18,47 +17,50 @@ const UserLoginPanel = () => {
             { status === "authenticated" 
             ?
                 <Menu as="div" className="relative inline-block text-left">
-                    <div>
+                    {({ open }) => (
+                    <>
                         <Menu.Button className="inline-flex justify-center w-full rounded-m px-4 py-2 bg-white text-sm font-medium text-gray-70 focus:outline-none">
                             {session.user.name}
-                            <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+                            <ChevronDownIcon 
+                            className={`-mr-1 ml-2 h-5 w-5 transform duration-100 ${open ? 'rotate-[180deg]' : ''}`} 
+                            aria-hidden="true" />
                         </Menu.Button>
-                    </div>
 
-                    <Transition
-                        as={Fragment}
-                        enter="transition ease-out duration-100"
-                        enterFrom="transform opacity-0 scale-95"
-                        enterTo="transform opacity-100 scale-100"
-                        leave="transition ease-in duration-75"
-                        leaveFrom="transform opacity-100 scale-100"
-                        leaveTo="transform opacity-0 scale-95"
-                    >
-                        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        <div className="py-1">
-                            <Menu.Item>
-                                {({ active }) => (
-                                    <CustomLink href='/profile' className={`block px-4 py-2 text-sm 
-                                    ${active 
-                                    ? 'bg-gray-100 text-gray-900' 
-                                    : 'text-gray-700'}`}>
-                                        Личный кабинет
-                                    </CustomLink>
-                                )}
-                            </Menu.Item>
-                            <Menu.Item>
-                                {({ active }) => (
-                                    <CustomLink href='/api/auth/signout' className={`block px-4 py-2 text-sm 
-                                    ${active 
-                                    ? 'bg-gray-100 text-gray-900' 
-                                    : 'text-gray-700'}`}>
-                                        Выйти
-                                    </CustomLink>
-                                )}
-                            </Menu.Item>
-                        </div>
-                        </Menu.Items>
-                    </Transition>
+                        <Transition
+                            show={open}
+                            enter="transition ease-out duration-100"
+                            enterFrom="transform opacity-0 scale-95"
+                            enterTo="transform opacity-100 scale-100"
+                            leave="transition ease-in duration-75"
+                            leaveFrom="transform opacity-100 scale-100"
+                            leaveTo="transform opacity-0 scale-95"
+                        >
+                            <Menu.Items static className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            <div className="py-1">
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <CustomLink href='/profile' className={`block px-4 py-2 text-sm 
+                                        ${active 
+                                        ? 'bg-gray-100 text-gray-900' 
+                                        : 'text-gray-700'}`}>
+                                            Личный кабинет
+                                        </CustomLink>
+                                    )}
+                                </Menu.Item>
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <CustomLink href='/api/auth/signout' className={`block px-4 py-2 text-sm 
+                                        ${active 
+                                        ? 'bg-gray-100 text-gray-900' 
+                                        : 'text-gray-700'}`}>
+                                            Выйти
+                                        </CustomLink>
+                                    )}
+                                </Menu.Item>
+                            </div>
+                            </Menu.Items>
+                        </Transition>
+                    </>)}
                 </Menu>
             : 
                 <CustomLink href='/api/auth/signin'>
