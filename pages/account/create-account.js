7e5e62d38123f9eabb114b0e79f71591dcb2779a
Ancubed <antonov.andrew.jr@gmail.com>
2022-02-06 23:56:27 +0500

    Простой формгенератор (пока текстовое поле и селект)

--- pages/account/create-account.js
+++ pages/account/create-account.js
@@ -1,7 +1,43 @@
+import CustomForm from '../../components/customForm'
+
 const CreateAccount = () => {
+
+    const fields = [
+        {
+            type: 'text',
+            id: 'name',
+            labelText: 'Имя',
+            required: true,
+            value: ''
+        },
+        {
+            type: 'text',
+            id: 'company',
+            labelText: 'Название компании',
+            required: true
+        },
+        {
+            type: 'select',
+            id: 'role',
+            labelText: 'Тип компании',
+            required: true,
+            options: [{
+                value: '0',
+                text: 'Администратор'
+            },{
+                value: '1',
+                text: 'Заказчик'
+            },{
+                value: '2',
+                text: 'Исполнитель'
+            }]
+        },
+    ]
+
     return (
         <main>
             <h1 className="text-2xl mb-4">Создать аккаунт</h1>
+            <CustomForm fields={fields} buttonText='Добавить' method='put' url='/api/accounts/create'/>
         </main>
     )
 }
