2c895085994a9cfd705d8ce1dc4ec4392e8cfdc1
Ancubed <antonov.andrew.jr@gmail.com>
2022-02-10 14:31:31 +0500

    отдельная страница edit

--- pages/accounts/[id]/edit.js
+++ pages/accounts/[id]/edit.js
@@ -1,15 +1,18 @@
 import { useSession, getSession } from 'next-auth/react'
 import Error from '../../../components/error'
-import CustomForm from '../../../components/customForm.tsx'
+import CustomForm from '../../../components/customForm'
 
-// import dbConnect from '../../lib/mongoose'
-// import UserModel from '../../models/User'
+import dbConnect from '../../../lib/mongoose'
+import UserModel from '../../../models/User'
 
-const EditAccount = (props) => {
+function EditAccount(props) {
     const { data: session } = useSession()
 
-    if (!session || !session.user)
+    if (!session || !session.user || session.user.role.id != 0 && session.user.id != props.account.id)
         return <Error errStatusCode={403} errMessage="Нет доступа" />
+
+    if (!props.account)
+        return <Error errStatusCode={404} errMessage="Аккаунт не существует" />
 
     const fields = [
         {
@@ -20,107 +23,108 @@ const EditAccount = (props) => {
             options: [
                 {
                     value: '0',
-                    text: 'Администратор',
+                    text: 'Администратор'
                 },
                 {
                     value: '1',
-                    text: 'Заказчик',
+                    text: 'Заказчик'
                 },
                 {
                     value: '2',
-                    text: 'Исполнитель',
+                    text: 'Исполнитель'
                 },
             ],
+            value: props.account.roleId,
+            disabled: true
         },
         {
             type: 'text',
             id: 'company',
             labelText: 'Название компании',
             required: true,
+            value: props.account.company
         },
         {
             type: 'text',
             id: 'surname',
             labelText: 'Фамилия',
             required: true,
-            value: '',
+            value: props.account.surname
         },
         {
             type: 'text',
             id: 'name',
             labelText: 'Имя',
             required: true,
-            value: '',
+            value: props.account.name
         },
         {
             type: 'text',
             id: 'patronymic',
             labelText: 'Отчество',
             required: true,
-            value: '',
+            value: props.account.patronymic
         },
         {
             type: 'email',
             id: 'email',
             labelText: 'Email',
             required: true,
-            value: '',
+            value: props.account.email
         },
         {
             type: 'password',
             id: 'password',
             labelText: 'Пароль',
-            required: true,
             value: '',
         },
         {
             type: 'password',
             id: 'repeatPassword',
             labelText: 'Повторите пароль',
-            required: true,
             value: '',
         },
     ]
 
+    console
+
     return (
         <main>
-            <h1 className="text-2xl mb-4">Создать аккаунт</h1>
+            <h1 className="text-2xl mb-4">Редактировать аккаунт</h1>
             <CustomForm
                 fields={fields}
-                buttonText="Добавить"
-                method="put"
-                url="/api/accounts/create"
-            />
+                buttonText="Изменить"
+                method="post"
+                url={`/api/accounts/${props.account.id}`} />
         </main>
     )
 }
 
 export async function getServerSideProps(context) {
-    // const { id } = context.query;
-    // let acc, data = null;
-    // if (id != 'create') {
-    //     await dbConnect();
-    //     try {
-    //         acc = await UserModel.findOne({ _id: id });
-    //     } catch (err) {
-    //         console.log('Неверный id');
-    //     }
-    //     let name = acc.name.split(' ');
-    //     if (acc) data = {
-    //         id: acc._id.toString(),
-    //         company: acc.company,
-    //         surname: name[0],
-    //         name: name[1],
-    //         patronymic: name[2] || '',
-    //         email: acc.company,
-    //         roleName: acc.role.name,
-    //         roleId: acc.role.id
-    //     }s
-    // }
+    const { id } = context.query;
+    let acc, data = null;
+    await dbConnect();
+    try {
+        acc = await UserModel.findOne({ _id: id });
+    } catch (err) {
+        console.log('Неверный id');
+    }
+    let name = acc.name.split(' ');
+    if (acc) data = {
+        id: acc._id.toString(),
+        company: acc.company,
+        surname: name[0],
+        name: name[1],
+        patronymic: name[2] || '',
+        email: acc.email,
+        roleName: acc.role.name,
+        roleId: acc.role.id.toString()
+    }
 
     return {
         props: {
             session: await getSession(context),
+            account: data
         },
     }
 }
