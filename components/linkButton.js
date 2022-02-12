6134b7f147fa0433051bf4b9a00c75182f22567a
Ancubed <antonov.andrew.jr@gmail.com>
2022-02-12 21:10:51 +0500

    Grow 0

--- components/linkButton.js
+++ components/linkButton.js
@@ -4,7 +4,7 @@ const LinkButton = (props) => {
     return (
         <CustomLink
             href={props.link}
-            className="flex grow justify-center mt-4 p-2 rounded border-2"
+            className="flex grow-0 justify-center mt-4 p-2 rounded border-2"
         >
             {props.text || 'Открыть'}
         </CustomLink>
