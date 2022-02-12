d0678545c49e8b61eb997f4d5558045aa5134e28
Ancubed <antonov.andrew.jr@gmail.com>
2022-02-12 21:11:08 +0500

    Рефакторинк

--- components/accountsLinksList.js
+++ components/accountsLinksList.js
@@ -4,19 +4,19 @@ import { useState, useEffect } from 'react'
 import AccountsLink from './accountsLink'
 import LinkButton from './linkButton'
 
-const fetcher = (...args) =>
-    fetch(...args)
-        .then((res) => res.json())
-        .then((json) => json.data)
+const fetcher = async (...args) => {
+    let res = await fetch(...args)
+    if (!res.ok) throw new Error('Error');
+    let json = await res.json();
+    if (!json.data) throw new Error(json.message || 'Неизвестная ошибка');
+    return json.data;
+}
 
 const AccountsLinksList = (props) => {
     const { data, error } = useSWR(`api/accounts`, fetcher)
     const [accounts, setAccounts] = useState(data)
 
     useEffect(() => { setAccounts(data); }, [data])
-
-    if (!accounts && !error) return <p>Загрузка...</p>
-    if (!accounts) return <p>Нет данных</p>
 
     const deleteAcc = (id) => {
         if (accounts && accounts.length != 0) {
@@ -27,14 +27,18 @@ const AccountsLinksList = (props) => {
     return (
         <div className="flex grow flex-col max-w-[50%]">
             <h3 className="text-lg mb-1">Аккаунты</h3>
-            <div>
+            {!accounts && !error && <p>Загрузка...</p>}
+            {!accounts && error && <p>Нет данных</p>}
+            {accounts && <div>
                 {accounts.map((acc, key) => {
                     return acc.id !== props.user.id ? (
                         <AccountsLink account={acc} key={key} user={props.user} deleteAcc={deleteAcc}/>
                     ) : null
                 })}
-            </div>
-            <LinkButton text="Добавить" link="accounts/create" />
+            </div>}
+            {props.user.role.id == 0 
+            && 
+            <LinkButton text="Добавить" link="/accounts/create" />}
         </div>
     )
 }
