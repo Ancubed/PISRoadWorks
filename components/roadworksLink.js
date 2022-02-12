a932990ef74289478da7bf91f5039faa95c41b90
Ancubed <antonov.andrew.jr@gmail.com>
2022-02-12 21:10:30 +0500

    Дорожные работы компонент

---
+++ components/roadworksLink.js
@@ -0,0 +1,51 @@
+import CustomLink from './customLink'
+
+const RoadworksLink = (props) => {
+
+    const handleDeleteLinkClick = async (event) => {
+        event.preventDefault();
+
+        if(confirm('Вы уверены, что хотите удалить аккаунт?')) {
+            let response = await fetch(`/api/roadworks/${props.account.id}`, {
+                method: 'delete',
+                headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+                }
+            });
+            if (response.ok) {
+                props.deleteAcc(props.account.id);
+            }
+        }
+    }
+
+    return (
+        <div className='p-4 border-2 rounded flex justify-between'>
+            <CustomLink
+                href={`/accounts/${props.account.id}`}
+                className="flex grow flex-col p-1 hover:text-teal-600"
+            >
+                <span className="mr-4">{props.account.company}</span>
+                <span className="mr-4">{props.account.name}</span>
+                <span className="mr-4">{props.account.role}</span>
+            </CustomLink>
+            {props.user.role.id == 0 
+            &&
+            <div>
+                <CustomLink
+                    href={`/accounts/${props.account.id}/edit`}
+                    className="p-1 hover:text-teal-600"
+                >
+                    <span>Р.</span>
+                </CustomLink>
+                <span
+                    className="p-1 cursor-pointer hover:text-teal-600"
+                    onClick={handleDeleteLinkClick}
+                >
+                    <span>У.</span>
+                </span>
+            </div>}
+        </div>
+    )
+}
+
+export default RoadworksLink
---
+++ components/roadworksLinksList.js
@@ -0,0 +1,46 @@
+import useSWR from 'swr'
+import { useState, useEffect } from 'react'
+
+import RoadworksLink from './roadworksLink'
+import LinkButton from './linkButton'
+
+const fetcher = async (...args) => {
+    let res = await fetch(...args)
+    if (!res.ok) throw new Error('Ошибка при выполнении запроса');
+    let json = await res.json();
+    if (!json.data) throw new Error(json.message || 'Неизвестная ошибка');
+    return json.data;
+}
+
+const RoadworksLinksList = (props) => {
+    const { data, error } = useSWR(`api/roadworks`, fetcher)
+    const [roadworks, setRoadworks] = useState(data)
+
+    useEffect(() => { setRoadworks(data); }, [data])
+
+    const deleteWork = (id) => {
+        if (roadworks && roadworks.length != 0) {
+            setAccounts(roadworks.filter(work => work.id != id));
+        }
+    }
+
+    return (
+        <div className="flex grow flex-col max-w-[50%]">
+            <h3 className="text-lg mb-1">Дорожные работы</h3>
+            {!roadworks && !error && <p>Загрузка...</p>}
+            {!roadworks && error && <p>Нет данных</p>}
+            {roadworks && <div>
+                {roadworks.map((work, key) => {
+                    return work.id !== props.user.id ? (
+                        <RoadworksLink work={work} key={key} user={props.user} deleteWork={deleteWork}/>
+                    ) : null
+                })}
+            </div>}
+            {props.user.role.id == 0 
+            && 
+            <LinkButton text="Добавить" link="/roadworks/create" />}
+        </div>
+    )
+}
+
+export default RoadworksLinksList
