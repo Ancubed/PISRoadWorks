32ad081e1ec244235d811a6e5e56cc6b29274967
Ancubed <antonov.andrew.jr@gmail.com>
2022-02-20 23:47:30 +0500

    фильтрация по исполнителю

--- components/roadworks/roadworksLinksList.js
+++ components/roadworks/roadworksLinksList.js
@@ -8,7 +8,9 @@ import LoadSpinner from '../common/loadSpinner'
 import { fetcher } from '../../lib/functions'
 
 const RoadworksLinksList = (props) => {
-    const { data, error } = useSWR(`/api/roadworks`, fetcher)
+    const { data, error } = useSWR(props.executor 
+        ? `/api/roadworks?executor=${props.executor}` 
+        : '/api/roadworks', fetcher)
     const [roadworks, setRoadworks] = useState(data)
 
     useEffect(() => { setRoadworks(data); }, [data])
@@ -20,18 +22,18 @@ const RoadworksLinksList = (props) => {
     }
 
     return (
-        <div className="flex grow flex-col max-w-[50%]">
-            <h3 className="text-lg mb-1">Дорожные работы</h3>
+        <div className={`flex grow flex-col ${props.className}`}>
+            <h2 className="text-xl mb-1">Дорожные работы</h2>
             {!roadworks && !error && <LoadSpinner />}
-            {!roadworks && error && <p>Нет данных</p>}
+            {!roadworks && error || roadworks?.length == 0 && !error && <p>Нет данных</p>}
             {roadworks && <div>
                 {roadworks.map((work, key) => {
-                    return work.id !== props.user.id ? (
+                    return work.id !== props.user?.id ? (
                         <RoadworksLink work={work} key={key} user={props.user} deleteWork={deleteWork}/>
                     ) : null
                 })}
             </div>}
-            {props.user.role.id == 0 
+            {props.user?.role?.id == 0 
             && 
             <LinkButton text="Добавить" link="/roadworks/create"/>}
         </div>
