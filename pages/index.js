24f9a6bad2d1980dcd2fd0f7382f1fdcb9d20e75
Ancubed <antonov.andrew.jr@gmail.com>
2022-03-27 15:02:04 +0500

    базовая верстка главной страницы

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,31 @@
+import { YMaps, Map, Polyline } from 'react-yandex-maps'
+import RoadWorksLinksList from '../components/roadworks/roadworksLinksList'
+
 const Home = () => {
     return (
         <main>
-            <h1 className="text-2xl mb-4">Главная</h1>
+            <h1 className="text-2xl mb-2">Карта дорожных работ</h1>
+            <YMaps>
+                <Map
+                    defaultState={{
+                        center: [51.786, 55.104],
+                        zoom: 11,
+                        autoFitToViewport: 'always'
+                    }}
+                    className='my-2 flex basis-3/4 grow lg:grow-0 h-96'
+                >
+                    {/* <Polyline
+                        geometry={props.roadwork.coordinates}
+                        options={{
+                            balloonCloseButton: true,
+                            strokeColor: '#000',
+                            strokeWidth: 4,
+                            strokeOpacity: 0.5
+                        }}
+                    /> */}
+                </Map>
+            </YMaps>
+            <RoadWorksLinksList status="actual" className='mt-4'/>
         </main>
     )
 }
