229ff5dd8b8dcd8a7a902b144e8c0a995e8517d3
Ancubed <antonov.andrew.jr@gmail.com>
2022-03-27 14:20:41 +0500

    frontend файлы

---
+++ components/common/customFiles.js
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+import Files from 'react-files'
+
+const CustomFiles = (props) => {
+    const [files, setFiles] = useState([])
+
+    function onFilesChange(newFiles) {
+        setFiles([...files, ...newFiles])
+        //console.log([...files, ...newFiles]);
+    }
+    
+    function onFilesError(error, file) {
+        alert('Ошибка при загрузке файла ' + error.code + ': ' + error.message)
+        console.log('Ошибка при загрузке файла ' + error.code + ': ' + error.message)
+    }
+
+    function filesRemoveOne(e) {
+        setFiles(files.filter(file => file.id !== e.target.dataset.fileid))
+    }
+
+    return (
+        <>
+            <Files
+                className='flex justify-center items-center border rounded-lg cursor-pointer mb-4 '
+                style={{ height: '50px' }}
+                onChange={onFilesChange}
+                onError={onFilesError}
+                accepts={props.accepts || ['.doc', '.docx', '.pdf', '.odt']}
+                multiple
+                maxFiles={10}
+                maxFileSize={10000000}
+                minFileSize={0}
+                clickable
+            >
+                Перенесите файлы сюда или кликните для загрузки
+            </Files>
+            {files && files.length > 0 
+            &&
+            <div className="flex flex-col">
+                {files.map((file, key) => 
+                    <div className='flex py-2' key={key}>
+                        <div className='flex justify-between flex-grow'>
+                            <span className='font-semibold'>{file.name}</span>
+                            <span className='font-light'>{file.sizeReadable}</span>
+                        </div>
+                        <div className='ml-8'>
+                            <span
+                                className="p-1 cursor-pointer hover:text-sky-600"
+                                onClick={filesRemoveOne}
+                                title='Удалить'
+                                data-fileid={file.id}
+                            >
+                                У.
+                            </span>
+                        </div>
+                    </div>
+                )}
+            </div>
+            }
+        </>
+    )
+}
+
+export default CustomFiles
---
+++ pages/roadworks/[id]/submit-document.js
@@ -0,0 +1,85 @@
+import { useSession, getSession } from 'next-auth/react'
+import Error from '../../../components/common/error'
+import CustomFiles from '../../../components/common/customFiles'
+
+import { YMaps, Map, Polyline } from 'react-yandex-maps';
+
+import RequestModel from '../../../models/Request'
+import RoadworkInfo from '../../../components/roadworks/roadworkInfo';
+
+import { dateFormatFromISO } from '../../../lib/functions'
+
+import { isAcceptByRole, isOwnerDataSession } from '../../../lib/functions'
+
+function SubmitDocuments(props) {
+    const { data: session } = useSession()
+
+    if (!props.roadwork)
+        return <Error errStatusCode={404} errMessage="Дорожная работа не существует"/>
+
+    if (!isAcceptByRole(session) && !isOwnerDataSession(session, props.roadwork.executorId))
+        return <Error errStatusCode={403} errMessage="Нет доступа" />
+
+    return (
+        <main>
+            <h1 className="text-2xl mb-4">{`Работа №${props.roadwork.id}`}</h1>
+            <div className="-m-2 flex flex-row flex-wrap lg:flex-nowrap">
+                <YMaps>
+                    <Map
+                        defaultState={{
+                            center: props.roadwork.coordinates[0] || [51.786, 55.104],
+                            zoom: 15,
+                            autoFitToViewport: 'always'
+                        }}
+                        className='m-2 flex basis-3/4 grow lg:grow-0 h-96'
+                    >
+                        <Polyline
+                            geometry={props.roadwork.coordinates}
+                            options={{
+                                balloonCloseButton: true,
+                                strokeColor: '#000',
+                                strokeWidth: 4,
+                                strokeOpacity: 0.5
+                            }}
+                        />
+                    </Map>
+                </YMaps>
+                <RoadworkInfo className='m-2 flex basis-1/4 grow lg:grow-0' roadwork={props.roadwork}/>
+            </div>
+            <div className=''>
+                <h1 className="text-2xl my-4">Подача документов</h1>
+                <CustomFiles />
+            </div>
+        </main>
+    )
+}
+
+export async function getServerSideProps(context) {
+    const { id } = context.query;
+    let work, data = null;
+    try {
+        work = await RequestModel.findOne({ _id: id });
+    } catch (err) {
+        console.log('Неверный id');
+    }
+    if (work) data = {
+        id: work._id.toString(),
+        executorId: work.executorId.toString(),
+        executorName: work.executorName,
+        coordinates: work.coordinates,
+        status: work.status,
+        adress: work.adress,
+        dateStart: dateFormatFromISO(work.dateOfStart?.toISOString()),
+        dateEnd: dateFormatFromISO(work.dateOfEnd?.toISOString()),
+        comment: work.comment
+    }
+
+    return {
+        props: {
+            session: await getSession(context),
+            roadwork: data
+        }
+    }
+}
+
+export default SubmitDocuments
