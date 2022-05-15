c56abe4f2ee58e855130171ff6d748e06a03a5bc
Ancubed <antonov.andrew.jr@gmail.com>
2022-05-15 18:31:43 +0500

    роут изменить статус

---
+++ pages/roadworks/[id]/change-status.js
@@ -0,0 +1,137 @@
+import { useSession, getSession } from 'next-auth/react'
+import { useState, useEffect } from 'react'
+import Router from 'next/router'
+
+import Error from '../../../components/common/error'
+import FormButton from '../../../components/common/formButton'
+
+import { YMaps, Map, Polyline } from 'react-yandex-maps';
+
+import RequestModel from '../../../models/Request'
+import RoadworkInfo from '../../../components/roadworks/roadworkInfo';
+
+import { dateFormatFromISO } from '../../../lib/functions'
+
+import { isAcceptByRole, isOwnerDataSession } from '../../../lib/functions'
+
+function ChangeStatus(props) {
+    const { data: session } = useSession()
+
+    const getResponse = async (status, comment = null) => {
+        let body = { status: status }
+        if (comment) body.comment = comment
+        return await fetch(`/api/roadworks/${props.roadwork.id}/change-status`, {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(body)
+        });
+    }
+
+    const onSubmit = async (status = 'done') => {
+        let response = await getResponse(status)
+        if (response.ok) {
+            let json = await response.json()
+            if (json.isSuccess) {
+                Router.push('/profile')
+            } else {
+                alert(json.message || 'Ошибка при смене статуса')
+            }
+        } else {
+            alert('Ошибка при смене статуса')
+        }
+    }
+
+    if (!props.roadwork)
+        return <Error errStatusCode={404} errMessage="Дорожная работа не существует"/>
+
+    if (!isAcceptByRole(session) && !isOwnerDataSession(session, props.roadwork.executorId))
+        return <Error errStatusCode={403} errMessage="Нет доступа" />
+
+    if (props.roadwork.status !== 'inProgress')
+        return <Error errStatusCode={403} errMessage="Нет доступа"/>
+
+    return (
+        <main>
+            <h1 className="text-2xl mb-4">{`Работа №${props.roadwork.id}`}</h1>
+            <div className="-m-2 flex flex-row flex-wrap lg:flex-nowrap">
+                <YMaps>
+                    <Map
+                        defaultState={{
+                            center: props.roadwork.coordinates[0] || [51.786, 55.104],
+                            zoom: 15,
+                            autoFitToViewport: 'always'
+                        }}
+                        className='m-2 flex basis-3/4 grow lg:grow-0 h-96'
+                    >
+                        <Polyline
+                            geometry={props.roadwork.coordinates}
+                            options={{
+                                balloonCloseButton: true,
+                                strokeColor: '#000',
+                                strokeWidth: 4,
+                                strokeOpacity: 0.5
+                            }}
+                        />
+                    </Map>
+                </YMaps>
+                <RoadworkInfo className='m-2 flex basis-1/4 grow lg:grow-0' roadwork={props.roadwork}/>
+            </div>
+            <div className=''>
+                <h1 className="text-2xl my-4">Документы</h1>
+                {props.roadwork.files && props.roadwork.files.length > 0
+                &&
+                    props.roadwork.files.map((file, idx) => 
+                        <div key={idx} className='flex justify-between'>
+                            <a href={`/api/files/${file._id}`} target="_blank" rel="noreferrer" className='block hover:text-sky-600'>
+                                {`${idx + 1}. ${file.filename}`}
+                            </a>
+                        </div>)
+                }
+            </div>
+            <div className='flex'>
+                <h1 className="text-2xl my-4">Изменить статус</h1>
+                <FormButton type="button" text='Выполнена' onClick={onSubmit} className='my-2'/>
+                <FormButton type="button" text='Просрочена' onClick={() => onSubmit('expired')} className='my-2'/>
+            </div>
+        </main>
+    )
+}
+
+export async function getServerSideProps(context) {
+    const { id } = context.query;
+    let work, data = null;
+    try {
+        work = await RequestModel.findOne({ _id: id });
+    } catch (err) {
+        console.log('Неверный id');
+    }
+    if (work) data = {
+        id: work._id.toString(),
+        executorId: work.executorId.toString(),
+        executorName: work.executorName,
+        coordinates: work.coordinates,
+        status: work.status,
+        adress: work.adress,
+        dateStart: dateFormatFromISO(work.dateOfStart?.toISOString()),
+        dateEnd: dateFormatFromISO(work.dateOfEnd?.toISOString()),
+        comment: work.comment,
+        rejectComment: work.rejectComment,
+        files: work.files?.map(file => { 
+            return {
+                _id: file._id.toString(),
+                filename: file.filename
+            }
+        })
+    }
+    
+    return {
+        props: {
+            session: await getSession(context),
+            roadwork: data
+        }
+    }
+}
+
+export default ChangeStatus
